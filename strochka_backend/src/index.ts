e679cb5d90a56b265a5a47fb7c10585a78c2a512
Daniil Zavarin <lovec_omarov@mail.ru>
2024-12-26 22:04:07 +0300

    feat: Added tRPC for backend

--- strochka_backend/src/index.ts
+++ strochka_backend/src/index.ts
@@ -1,22 +1,18 @@
 import express from 'express';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRouter } from './trpc';
 
 const expressApp = express();
 
 const serverPort = 3000;
 
-const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
-  { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
-  { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
-  { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
-  { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
-];
-
 expressApp.listen(serverPort, () => {
   console.info(`Listening at http://localhost:${serverPort}`);
 });
 
-expressApp.get('/ideas', (req, res) => {
-  res.send(ideas);
-});
-
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  }),
+);
