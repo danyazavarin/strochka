c0724c6b8db3db845be5e262c964798f48538cbd
Daniil Zavarin <lovec_omarov@mail.ru>
2024-12-26 22:54:29 +0300

    feat: Added tRPC for frontend

--- strochka_frontend/src/App.tsx
+++ strochka_frontend/src/App.tsx
@@ -1,14 +1,10 @@
+import { TrpcProvider } from './lib/trpc';
+import { AllIdeasPage } from './pages/allIdeasPage';
+
 export const App = () => {
-  
   return (
-    <div>
-      <h1>Ideanick</h1>
-      {ideas.map((idea) => (
-        <div key={idea.nick}>
-          <h2>{idea.name}</h2>
-          <p>{idea.description}</p>
-        </div>
-      ))}
-    </div>
+    <TrpcProvider>
+      <AllIdeasPage />
+    </TrpcProvider>
   );
 };
